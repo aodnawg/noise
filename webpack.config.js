2513c7592e7e07d351f7ec73f9d82b7b544be7e0
adawong <mortuary.12@gmail.com>
2017-11-26 19:41:14 +0900

    add build option for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
+const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
+module.exports = [{
   entry: './src/js/index.js',
-
-  output: {
-    filename: 'noise.js',
-    path: __dirname
-  },
-
+  output:
+    { filename: 'noise.js',
+      path: path.join(__dirname, 'dist') },
   module: {
     rules: [{
       test: /\.js$/,
@@ -20,6 +18,27 @@ module.exports = {
       }],
     }],
   },
-
   devtool: 'inline-source-map'
-};
+},
+
+{
+  entry: './src/js/index.js',
+  output:
+    { filename: 'noise.js',
+      path: path.join(__dirname, 'dist') },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }],
+    }],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({ compress: { screw_ie8: true, warnings: false } })
+  ],
+}];
